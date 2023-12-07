c9125f4466775e6442445eb9f52b9b087c05ec11
Rafic <KattourR@adx.ae>
2023-12-07 22:43:07 +0200

    changing vite target

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://rafickattour.github.io',
+        target: 'http://rafickattour.github.io/appointment-creation',
         changeOrigin: true
       },
     },
